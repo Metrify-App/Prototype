9f85f0a43bd8acecec3bf04c96704abf4ab170e9
Antoine <antoine@shaptwn.com>
2026-03-09 01:23:43 +0100

    feat: add formatDate utils function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateStr: string) {
+    return new Date(dateStr).toLocaleDateString('fr-FR', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric'
+    });
+}
